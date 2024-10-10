7b05e3b81f3d866f0a2725651828546b8f9a9575
Gu <gu.mivvi@gmail.com>
2024-10-10 00:49:53 -0300

    add

--- MVC/Conversores de valores/projetoconversor/projetoconversor/Models/ResultadoViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace projetosoma.Models
-{
-    public class ResultadoViewModel
-    {
-        public double ValorMetros { get; set; }
-        public string Unidade { get; set; }
-        public double Resultado { get; set; }
-    }
-}
